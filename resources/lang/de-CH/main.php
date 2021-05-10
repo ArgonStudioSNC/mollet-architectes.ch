c79bd0ef048e3e82b6eaa15ae6916d8a84f0b39a
Charrière Loïc <loic.charriere@students.bfh.ch>
2021-05-10 14:24:36 +0200

    corrected date

--- resources/lang/de-CH/main.php
+++ resources/lang/de-CH/main.php
@@ -14,7 +14,7 @@ return [
     'competitions' => 'Wettbewerbe',
     'office' => 'Agentur',
 
-    'description-home' => "Ein Archiv der Wettbewerbe und Leistungen des Bieler Architekturbüros « Henri Mollet Architectes Associés SA », von 1982 bis 2018.",
+    'description-home' => "Ein Archiv der Wettbewerbe und Leistungen des Bieler Architekturbüros « Henri Mollet Architectes Associés SA », von 1962 bis 2018.",
     'description-competitions' => "Wichtigste Leistungen und Wettbewerbe des Bieler Architekturbüros « Henri Mollet Architectes Associés SA » von 1982 bis 1997.",
     'description-office' => "Die immer komplexer werdenden Anforderungen an die Architektur verlangen den Beizug zahlreicher Fachleute, die durch den Architekten keinesfalls ersetzt werden können. Dennoch bleibt der Architekt, unabhängig von der technischen Entwicklung, die einzige Fachkraft, die direkt für eine umfassende Beurteilung der Probleme im Zusammenhang mit der räumlichen Gestaltung und der Konzeption von Projekten befähigt ist, also für die Umsetzung des vom Bauherrn vorge­gebenen Raumprogramms. Die Tatsache, dass die Erarbeitung von Projekten zu einer interdisziplinären Tätigkeit geworden ist, erfordert mehr denn je kreative Vorstellungskraft. Nur sie ist imstande, dem Projekt Sinn und Wert zu vermitteln, aber erst nachdem mit sämtlichen Spezialisten alle zur Verfügung stehenden Mittel geprüft wurden, die eine gesamtheitliche Beantwortung der gestellten Fragen ermöglichen.",
 
--- resources/lang/fr-CH/main.php
+++ resources/lang/fr-CH/main.php
@@ -14,7 +14,7 @@ return [
     'competitions' => 'concours',
     'office' => 'agence',
 
-    'description-home' => "Une archive des concours et des réalisations du bureau d’architecture biennois « Henri Mollet Architectes Associés SA », de 1982 à 2018.",
+    'description-home' => "Une archive des concours et des réalisations du bureau d’architecture biennois « Henri Mollet Architectes Associés SA », de 1962 à 2018.",
     'description-competitions' => "Principales réalisations et concours du bureau d’architecture biennois « Henri Mollet Architectes Associés SA » de 1982 à 1997.",
     'description-office' => "La fonction architecturale, devenue de plus en plus complexe, nécessite l’intervention de compétences techniques diverses de plus en plus nombreuses auxquelles l’architecte ne prétend en aucun cas se substituer. Cependant, l’architecte demeure, quelle que soit l’évolution des techniques, le seul professionnel directement préparé à l’appréhension globale des problèmes de l’aménagement de l’espace et à la conception des projets, c’est-à-dire à la traduction en volume des programmes définis par le maître d’ouvrage. Le fait même que l’établissement des projets soit devenu un acte pluridiscipli­naire nécessite plus que jamais l’intervention d’une imagination créatrice seule capable de donner au projet son sens et sa valeur, acte qui doit, après avoir fait avec l’ensemble des disciplines concernées l’examen des moyens, donner une réponse globale aux problèmes posés.",
 
